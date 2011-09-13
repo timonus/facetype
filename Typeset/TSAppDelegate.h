a7204dc29cf591394caf7f8931e8fb0865b5bc2e
Tim Johnsen <timonus@gmail.com>
2011-09-12 20:00:52 -0700

    Initial Commit

---
+++ Typeset/TSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TSAppDelegate.h
+//  Typeset
+//
+//  Created by Tim Johnsen on 9/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Typeset/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Typeset
+//
+//  Created by Tim Johnsen on 9/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([TSAppDelegate class]));
+	}
+}
